ece9c8a3851b9a0fcfe00d57a3d3ba13f31cb39c
alicat99 <alicat9912374@gmail.com>
2024-06-05 22:45:01 +0900

    https6

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   devServer: {
     https: {
-      key: fs.readFileSync('keys/privkey1.pem'),
-      cert: fs.readFileSync('keys/cert1.pem'),
+      key: fs.readFileSync('keys/key.pem'),
+      cert: fs.readFileSync('keys/cert.pem'),
     },
   },
   transpileDependencies: true
