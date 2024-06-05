6f8a1481cc624da013f840f7b681057f34bad0b6
alicat99 <alicat9912374@gmail.com>
2024-06-05 22:18:03 +0900

    https

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
+const fs = require('fs');
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    https: {
+      key: fs.readFileSync('/etc/letsencrypt/live/soongsil.us/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/soongsil.us/cert1.pem'),
+    },
+  },
   transpileDependencies: true
 })
