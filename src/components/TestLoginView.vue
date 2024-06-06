c263a5aa9848400320bd1d7d6b27583d64f15f54
alicat99 <alicat9912374@gmail.com>
2024-06-07 00:32:36 +0900

    login

---
+++ src/components/TestLoginView.vue
@@ -0,0 +1,36 @@
+<template>
+  <button @click="login">button</button>
+</template>
+
+<script setup>
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// Firebase Auth 서비스 참조
+const auth = firebase.auth();
+
+function login() {
+  const email = "alicat9912374@gmail.com";
+  const password = "a1b2c3";
+  console.log('login');
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // 로그인 성공
+      const user = userCredential.user;
+
+      // ID 토큰 가져오기
+      user.getIdToken().then((idToken) => {
+        console.log('ID 토큰:', idToken);
+      });
+    })
+    .catch((error) => {
+      // 로그인 실패
+      console.error('로그인 오류:', error);
+    });
+}
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfZdlz9mGnYtB8NGwgdKFP1n6yYwIr1DU",
+  authDomain: "soongsil-af5dc.firebaseapp.com",
+  projectId: "soongsil-af5dc",
+  storageBucket: "soongsil-af5dc.appspot.com",
+  messagingSenderId: "462824179207",
+  appId: "1:462824179207:web:aeaa6e3c19deae124c0ca2",
+  measurementId: "G-XRL5W54Y7M"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const app = createApp(App)
 app.use(router)
